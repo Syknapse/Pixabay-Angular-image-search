543fa95634fd6ca6cc200eb5d2f156726ec2a3cc
Syk Houdeib <syk.houdeib@gmail.com>
2017-10-08 13:02:54 +0200

    Complete project
    copied the complete project files into repo.

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ImageListComponent } from './image-list/image-list.component';
+
+import { ImageService } from './shared/image.service';
+
+import { MatInputModule, MatButtonModule, MatProgressBarModule, MatCardModule } from '@angular/material';
+import { MasonryModule } from 'angular2-masonry';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImageListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+	HttpModule,
+	MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MasonryModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [ImageService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
